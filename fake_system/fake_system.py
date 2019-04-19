4b6a045a3e36baa996a70db925bdfa8cda79616e
jianhuizhai <jianhuizhai89@gmail.com>
2019-04-19 12:51:03 +0200

    Add files via upload

--- fake_system/fake_system.py
+++ fake_system/fake_system.py
@@ -1,9 +1,25 @@
+"""
+This code is used to visulization of energy by using dumpfile.
+The energy value of deleted atoms are assigned to 5.
+The energy value of not considered atoms are assigned to 20.
+"""
 import numpy as np
 import sys
 import linecache
 from output_fake import DumpOutput
 
 linecommon = '========================================================='
+
+print(linecommon)
+n = int(input("How many atoms are deleted : "))
+ions=[]
+while (n>0):
+    ions.append(input("atom id : "))
+    n = n-1
+
+ions = np.array( ions, dtype = int )  # change list to int array. 
+print(ions)
+
 
 lmp_file    = sys.argv[1]
 energy_file = sys.argv[2]
@@ -26,16 +42,22 @@ skipline  = 16
 atom_id, atom_type, x,y,z = np.loadtxt(lmp_file, skiprows=skipline, usecols=(0,1,2,3,4), unpack=True)
 eng_id, eng_eng = np.loadtxt(energy_file, usecols=(0,5), unpack=True)
 
-Eng = np.zeros(len(atom_id))
+'''
+print("type(atom_id) : ", type(atom_id))
+print("type(ions) : ", type(ions))
+'''
 
+Eng = np.ones(len(atom_id))*20
 #print("Eng", Eng)
 
+
 for i in range(len(atom_id)):
+    for k in range(len(ions)):
+        if(atom_id[i] == ions[k]):
+            Eng[i] = 5
     for j in range(len(eng_id)):
         if( atom_id[i] == eng_id[j] ):
             Eng[i] = eng_eng[j]
-            break
-        else:
-            Eng[i] = 20
 
 DumpOutput('dump.eng',xlim, ylim, zlim, atom_id, atom_type, x, y, z, Eng)
+print("Output file: dump.eng")
