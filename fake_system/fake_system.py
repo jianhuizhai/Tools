d008cb5b7dd6e5ebd814f74100ad011a2b3bd2d2
jianhuizhai <jianhuizhai89@gmail.com>
2019-04-24 14:23:41 +0200

    Add files via upload

--- fake_system/fake_system.py
+++ fake_system/fake_system.py
@@ -21,8 +21,8 @@ while (n>0):
 ions = np.array( ions, dtype = int )  # change list to int array. 
 '''
 ions = np.loadtxt("deleted.dat", usecols=(0))
-print("The already deleted ions are : ")
-print(ions)
+print("The already deleted %i ions are : "  %ions.size)
+print( ions )
 
 
 lmp_file    = sys.argv[1]
@@ -46,6 +46,7 @@ skipline  = 16
 atom_id, atom_type, x,y,z = np.loadtxt(lmp_file, skiprows=skipline, usecols=(0,1,2,3,4), unpack=True)
 eng_id, eng_eng = np.loadtxt(energy_file, usecols=(0,6), unpack=True)
 
+atom_id = list(atom_id)
 '''
 print("type(atom_id) : ", type(atom_id))
 print("type(ions) : ", type(ions))
@@ -54,14 +55,32 @@ print("type(ions) : ", type(ions))
 Eng = np.ones(len(atom_id))*20
 #print("Eng", Eng)
 
+if(ions.size != 0):
+    if( ions.size == 1):
+        id = atom_id.index(ions)
+        Eng[id] = 5
+    else:
+        id = [atom_id.index( k ) for k in ions]
+        Eng[id] = 5
+for k in range(eng_id.size):
+    id = atom_id.index( eng_id[k] )
+    Eng[id] = eng_eng[k]
 
+'''
+# this is the older version assign energy value
 for i in range(len(atom_id)):
-    for k in range(len(ions)):
-        if(atom_id[i] == ions[k]):
-            Eng[i] = 5
+    if(ions.size != 0):
+        if(ions.size == 1):
+            if(atom_id[i] == ions):
+                Eng[i] = 5
+        else:
+            for k in range(ions.size):
+                if(atom_id[i] == ions[k]):
+                    Eng[i] = 5
     for j in range(len(eng_id)):
         if( atom_id[i] == eng_id[j] ):
             Eng[i] = eng_eng[j]
+'''
 
 DumpOutput('dump.eng',xlim, ylim, zlim, atom_id, atom_type, x, y, z, Eng)
 print("Output file: dump.eng")
