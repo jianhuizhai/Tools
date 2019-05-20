b4e99aed8a9c65917b425874cca1641963abadae
jianhuizhai <jianhuizhai89@gmail.com>
2019-05-20 13:04:14 +0200

    determine whether the file exists or not before deleting

--- deleteUselessFolderDumpfile.py
+++ deleteUselessFolderDumpfile.py
@@ -26,7 +26,7 @@ if flag == '1':
                     print('deleting dumpfiles in folder : %10s.' %folder)
                 os.chdir( folder )
                 os.system('rm -f dump.relax*')
-                os.system('rm -f *.out')
+                os.system('rm -f *.lmp *.out')
                 os.chdir('../')
 
 elif flag == '2' :
@@ -41,6 +41,7 @@ elif flag == '2' :
                     print( energy_infoPath )
                     energy_infoFolders = np.loadtxt(os.path.join(energy_infoPath, 'energy_info.dat'), dtype= int ,usecols=([0]) ) 
                     keepFolder         = str( energy_infoFolders[0] )
+
                     for folder in os.listdir( energy_infoPath ):
                         #print('things in %s : %s' %(energy_infoPath,folder) )
                         if os.path.isdir( os.path.join(energy_infoPath,folder) ):  # import. It will only show part of folders in this folder is use os.path.isdir(folder)
@@ -50,21 +51,25 @@ elif flag == '2' :
                                 if flag_info == 'y' :
                                     print(linecommon)
                                     print( "{}{:10s}".format('The dumpfiles in this folder are keeped.  ',   folder) )
-        
+
+                            # delete files in folder : reference
                             elif folder == 'reference' :
                                 # keep the last dump file in order to have a look at config
                                 if flag_info == 'y' :
                                     print( linecommon )
                                     print( 'deleting files in folder : reference')
-                                os.remove( os.path.join(energy_infoPath, folder, 'initial.lmp') )
-                                os.remove( os.path.join(energy_infoPath, folder, 'dump.relax0') )
+                                for filename in os.listdir( os.path.join(energy_infoPath, folder) ):
+                                    if filename.endswith('.lmp') or filename == 'dump.relax0':
+                                        if flag_info == 'y':
+                                            print( filename )
+                                        os.remove( os.path.join(energy_infoPath, folder, filename) )
+                            
+                            # delete files in other folders
                             else:
                                 if flag_info == 'y':
                                     print(linecommon)
                                     print('deleteing dumpfiles in folder : %10s' %folder )
                                 
-                                # delete initial.lmp in the folder
-                                os.remove( os.path.join(energy_infoPath, folder, 'initial.lmp'))
                                 
                                 for filename in os.listdir( os.path.join(energy_infoPath, folder) ):
                                     if filename.startswith('dump.relax'):
@@ -73,6 +78,12 @@ elif flag == '2' :
                                         os.remove( os.path.join(energy_infoPath, folder, filename) )
                                     
                                     if filename.startswith('slurm'):
+                                        if flag_info == 'y':
+                                            print( filename )
+                                        os.remove( os.path.join(energy_infoPath, folder, filename) )
+                                    
+                                    # delete .lmp file in the folder
+                                    if filename.endswith('.lmp') :
                                         if flag_info == 'y':
                                             print( filename )
                                         os.remove( os.path.join(energy_infoPath, folder, filename) )
