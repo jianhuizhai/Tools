a7ccac6da8a04938075ca86a0e8f5881f3b9f64b
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-19 12:54:53 +0200

    unpdate some commands (it can be used together with zip_unzip.py)

--- deleteUselessFolderDumpfile.py
+++ deleteUselessFolderDumpfile.py
@@ -9,7 +9,6 @@ print( linecommon )
 flag      = input("delete just subfolders (1) or walk through subfolders (2) : ").lower()
 flag_info = input("Do you want to show the info about deleted files (y or n) : ").lower()
 flag_keep = input("Do you want to keep the negative binding energy (1) or just the most favorable one (2) (1 or 2) : ")
-flag_lmp  = input("Do you want to delete lmp file in keepFolders (y or n) : ").lower()
 
 print( linecommon )
 
@@ -31,9 +30,8 @@ if flag == '1':
                 print( "The dumpfiles in this folder are keeped : {:10s}".format(folder) )
                 
                 os.chdir( folder )
-                os.system('rm -f dump.relax0 slurm*')
-                if flag_lmp == 'y':
-                    os.system( 'rm -f *.lmp' )
+                os.system('rm -f slurm*')
+                os.system('rm -f dump_diff* dump_both* dump_norm*')
                 os.chdir( '../' )
         
             elif folder != 'reference' and folder != '__pycache__' :
@@ -43,14 +41,6 @@ if flag == '1':
                 
                 os.chdir( folder )
                 os.system('rm -f dump.relax* slurm* *.lmp')
-                os.chdir('../')
-            
-            elif folder == 'reference':
-                if flag_info == 'y':
-                    print(linecommon)
-                    print('deleting files in folder : {:10s}'.format(folder) )
-                os.chdir( folder )
-                os.system( 'rm -f dump.relax0 slurm* *.lmp' )
                 os.chdir('../')
 
 elif flag == '2' :
@@ -79,7 +69,7 @@ elif flag == '2' :
                         os.system('python ~/bin/extractData_binding_energy.py')
                         os.chdir( cwd )
                     
-                    
+
                     print( linecommon )
                     print( energy_infoPath )
 
@@ -122,27 +112,14 @@ elif flag == '2' :
                                     print( "The dumpfiles in this folder are keeped : {:10s}".format(folder) )
                                 for filename in os.listdir( os.path.join(energy_infoPath, folder) ):
                                     if filename == 'dump.relax0' or filename.startswith('slurm'):
+                                        os.remove( os.path.join(energy_infoPath, folder, filename) )  
+                                    
+                                    # delete dump_diff.cfg, dump_diff.xsf, dump_both.xsf and dump_norm.dat
+                                    if filename.startswith('dump_diff') or filename.startswith('dump_both') or filename.startswith('dump_norm') :
                                         os.remove( os.path.join(energy_infoPath, folder, filename) )
-                                
-                                    if flag_lmp == 'y' and filename.endswith('.lmp') :
-                                        os.remove( os.path.join(energy_infoPath, folder, filename) )                                        
 
-                            # delete files in folder : reference
-                            elif folder == 'reference' :
-                                # keep the last dump file in order to have a look at config
-                                if flag_info == 'y' :
-                                    print( linecommon )
-                                    print( 'deleting files in folder : reference')
-                                for filename in os.listdir( os.path.join(energy_infoPath, folder) ):
-                                    if filename.endswith('.lmp') or filename == 'dump.relax0':
-                                        if flag_info == 'y':
-                                            print( filename )
-                                        os.remove( os.path.join(energy_infoPath, folder, filename) )
-                                    if filename.startswith('slurm'):
-                                        os.remove( os.path.join(energy_infoPath, folder, filename) )
-                            
                             # delete files in other folders
-                            else:
+                            elif folder != 'reference':
                                 if flag_info == 'y':
                                     print(linecommon)
                                     print('deleteing dumpfiles in folder : %10s' %folder )
@@ -157,9 +134,11 @@ elif flag == '2' :
                                         if flag_info == 'y':
                                             print( filename )
                                         os.remove( os.path.join(energy_infoPath, folder, filename) )
-                                    
+
                                     # delete .lmp file in the folder            
                                     if filename.endswith('.lmp'):
                                         if flag_info == 'y':
                                             print( filename )
                                         os.remove( os.path.join(energy_infoPath, folder, filename) )
+                                    
+                                    
