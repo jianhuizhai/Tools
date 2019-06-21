74f0f6e0ecf8f77cc2d546eb1b51b486810f2984
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-21 13:02:39 +0200

    correct os.walk command

--- zip_unzip.py
+++ zip_unzip.py
@@ -3,13 +3,14 @@
 import os
 import zipfile
 import time
+import numpy as np
 
 linecommont = '='*80
 
 print( linecommont )
 flag     = input(""">>>>>>>>>   just subfolders --- 1 
 >>>>>>>>>   walk through subfolders --- 2 
->>>>>>>>>   1 or 2 : """).lower()
+>>>>>>>>>   1 or 2 : """)
 
 print( linecommont )
 flag_zip = input('''>>>>>>>>>   zip --- 1
@@ -25,15 +26,16 @@ if flag_zip == '2' :
 begin = time.time()
 
 
- 
+
 if flag == '1':
+
     for folder in os.listdir('.'):
-        if os.path.isdir( folder ):
+        if os.path.isdir( folder ) :
             print( linecommont )
             print( 'folder : {}'.format( folder ) )
             os.chdir( folder )
             #os.system('rm -r home')
-            # zip
+            # zip all folders
             if flag_zip == '1' :
                 if os.path.exists( zip_filename ):
                     print('{} exists.'.format( zip_filename ) )
@@ -56,9 +58,12 @@ if flag == '1':
                 # unzip
                 if not os.path.exists( zip_filename ):
                     print('{} does not exist.'.format( zip_filename ) )
-                else:
+                
+                # only unzip results.zip in keepFolders which binding energy is negative
+                # vacancy is favorable to bind to the positive instead of staying in buck
+                else :
                     with zipfile.ZipFile('results.zip', 'r') as zip :
-                        print('unzip files : {}'.format( zip.namelist() ) )
+                        print('unzip files : \n{}'.format( zip.namelist() ) )
                         zip.extractall(path='.')
                 
                     if flag_rm == 'y' :
@@ -72,43 +77,43 @@ elif flag == '2' :
     print( 'current folder : \n', path )
     
     for folderName, subfolders, filenames in os.walk( path ):
-        for subfolder in subfolders:
-            if subfolder == 'v_mg' or subfolder == 'v_o':
-                energy_infoPath = os.path.join(path, folderName, subfolder )
-                print( linecommont )
-                print( 'subfolder : {} '.format(energy_infoPath) )
-
-                for folder in os.listdir( energy_infoPath ) :
-                    if os.path.isdir( os.path.join(energy_infoPath,folder) ):
-                        os.chdir( os.path.join( energy_infoPath, folder) )
-                        print( 'folder : {}'.format( folder ) )
-                        # zip
-                        if flag_zip == '1' :
-                            if os.path.exists( zip_filename ):
-                                print('{} exists.'.format( zip_filename ) )
-                            else:
-                                with zipfile.ZipFile( 'results.zip', 'w', compression=zipfile.ZIP_DEFLATED) as zip:
-                                    for filename in os.listdir( '.' ):
-                                        if filename.endswith('lmp') or filename.endswith('.sh'):
-                                            zip.write( filename )
-                                            os.remove( filename )
-                                        if filename == 'log.lammps':
-                                            zip.write( filename )
-                                        if filename.startswith('dump.relax'):
-                                            zip.write( filename )
-                                            os.remove( filename )
-                                        if filename.startswith('slurm'):
-                                            os.remove( filename )
-                                #print( zip.namelist() )
-                        # unzip
-                        elif flag_zip == '2' :
+        workPath = os.path.join( path, folderName)
+        os.chdir( workPath )
+        print( 'folder : {}'.format( workPath ) )
+        # zip all folders
+        if flag_zip == '1' :
+            if os.path.exists( zip_filename ):
+                print('{} exists.'.format( zip_filename ) )
+            else:
+                with zipfile.ZipFile( 'results.zip', 'w', compression=zipfile.ZIP_DEFLATED) as zip:
+                    for filename in os.listdir( '.' ):
+                        if filename.endswith('lmp') or filename.endswith('.sh'):
+                            zip.write( filename )
+                            os.remove( filename )
+                        if filename == 'log.lammps':
+                            zip.write( filename )
+                        if filename.startswith('dump.relax'):
+                            zip.write( filename )
+                            os.remove( filename )
+                        if filename.startswith('slurm'):
+                            os.remove( filename )
+            #print( zip.namelist() )
+        # unzip
+        elif flag_zip == '2' :
+            if not os.path.exists( zip_filename ):
+                print('{} does not exists.'.format( zip_filename ) )
                             
-                            with zipfile.ZipFile('results.zip', 'r') as zip :
-                                zip.extractall(path='.')
-                            if flag_rm == 'y' :
-                                os.remove( zip_filename )
-    os.chdir(path)
+            else :
+                with zipfile.ZipFile('results.zip', 'r') as zip :
+                    zip.extractall(path='.')
+                if flag_rm == 'y' :
+                    os.remove( zip_filename )
+        os.chdir(path)
 
+if flag_zip == '1':
+    print('zip files')
+else:
+    print('unzip files.')
 end = time.time()
 print( linecommont )
-print('running time is : {}s'.format( (end-begin)/60. ) )
+print('running time is : {:10.2f} mins '.format( (end-begin)/60. ) )
